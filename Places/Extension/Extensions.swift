61f9e0665d00b14cb46e83e0e95e8bc605685da7
Raul Samuel Quispe Mamani <raul.quispe@icloud.com>
2018-04-09 18:52:57 -0500

    Styles and Extensions

---
+++ Places/Extension/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  Places
+//
+//  Created by NARA on 4/9/18.
+//  Copyright © 2018 Ministerio de Transportes y Comunicaciones. All rights reserved.
+//
+
+import UIKit
+
+class Extensions: NSObject {
+
+}
---
+++ Places/Styles/Styles.swift
@@ -0,0 +1,13 @@
+//
+//  Styles.swift
+//  Places
+//
+//  Created by NARA on 4/9/18.
+//  Copyright © 2018 Ministerio de Transportes y Comunicaciones. All rights reserved.
+//
+
+import UIKit
+
+class Styles: NSObject {
+
+}
